82ec0ad8a7517e8be79ee19a779a1354a7026e5f
Andrey Mamontov <andrey.mamontov@ctco.lv>
2016-11-17 16:51:48 +0200

    API definition

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Lightweight Swift implementation of Inversion of Control Container
+
+```Swift
+public class A {
+}
+
+Container().register(A.self, creator: {A()})
+var a = Container().resolve(A.self)
+```
--- Sources/ContainerSwift.swift
+++ Sources/ContainerSwift.swift
@@ -1,4 +1,19 @@
 public class ContainerSwift {
 
+    let list:[String];
+
     var text = "Hello, World!"
+
+    public init() {
+        list = [];
+    }
+    
+    func register<T>(_ t: T.Type, creator:() -> T)  {
+        print("[\(t)]")
+    }
+    
+    public func resolve<T>(_ t: T.Type) -> T? {
+        print("[\(t)]")
+        return nil
+    }
 }
