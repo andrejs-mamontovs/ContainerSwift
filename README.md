d7697abb7baa3e648b2bfad1a1370d6c31dc0f72
Andrey Mamontov <andrey.mamontov@ctco.lv>
2016-11-18 16:08:53 +0200

    Complete container basic api

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Lightweight Swift implementation of Inversion of Control Container
 
 ```Swift
-public class A {
+public protocol AP {
+    var name: String {get}
 }
 
-Container().register(A.self, creator: {A()})
-var a = Container().resolve(A.self)
+public class A : AP {
+    public var name: String = "Test"
+}
+
+var container = Container()
+
+container.register(AP.self, creator: { _ in A()})
+
+var instance = container.resolve(AP.self)!
+
 ```
--- Sources/ContainerSwift.swift
+++ Sources/ContainerSwift.swift
@@ -1,19 +1,56 @@
-public class ContainerSwift {
+public class ContainerSwift : Resolver {
 
-    let list:[String];
-
-    var text = "Hello, World!"
-
-    public init() {
-        list = [];
+    var list = [Index: Any]()
+    
+    public func register<T>(_ t: T.Type, creator:(Resolver) -> T) {
+        // create record
+        list[Index(type: t)] = Value<T>(creator: creator)
     }
     
-    func register<T>(_ t: T.Type, creator:() -> T)  {
-        print("[\(t)]")
-    }
-    
+    typealias FactoryType = (Resolver) -> T
+
     public func resolve<T>(_ t: T.Type) -> T? {
-        print("[\(t)]")
-        return nil
+        return resolveInternal(index: Index(type: t)) {
+            (creator: FactoryType) in creator(self)
+        }
     }
+    
+    func resolveInternal<T, F>(index: Index, caller: (F)-> T) -> T? {
+        // cast value
+        let v = list[index] as? Value<T>
+        return caller(v?.creator as! F)
+    }
+    
+    class Index : Hashable, Equatable {
+        
+        let type : FunctionType.Type;
+        
+        public init(type: FunctionType.Type) {
+            self.type = type;
+        }
+        
+        public var hashValue: Int {
+            get {
+                return String(describing: type).hashValue
+            }
+        }
+        public static func ==(lhs: Index, rhs: Index) -> Bool {
+            return lhs.type == rhs.type;
+        }
+    }
+}
+
+typealias FunctionType = Any
+
+public class Value<T> {
+    
+    let creator: FunctionType
+    
+    init(creator: FunctionType) {
+        self.creator = creator
+    }
+}
+
+public protocol Resolver {   
+    func resolve<T>(_ t: T.Type) -> T?
 }
